163b5d113ff0b27decf82c4d4129c3bfbf6aa1fa
jimk100 <55141616+jimk100@users.noreply.github.com>
2020-02-20 18:06:12 -0500

    Add files via upload

---
+++ Assets/Assets/Shooter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject prefabUFO;
+
+    void Start()
+    {
+        GameObject UFO = Instantiate<GameObject>(prefabUFO);
+        Vector3 pos = Vector3.zero;
+        pos.x = -30;
+        UFO.transform.position = pos;
+    }
+}
---
+++ Assets/Assets/UFO.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject prefabLaser;
+    public float projectileSpeed;
+
+    [Header("Set in Inspector")]
+    public GameObject projectile;
+    public GameObject firepoint;
+    public Vector3 firepos;
+
+    void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.y = mousePos3D.y;
+        this.transform.position = pos;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireLaser();
+        }
+
+        void FireLaser()
+        {
+            projectile = Instantiate(prefabLaser) as GameObject;
+            projectile.transform.position = transform.position;
+            Rigidbody LaserRigid = projectile.GetComponent<Rigidbody>();
+            LaserRigid.velocity = Vector3.right * projectileSpeed;
+
+        }
+    }
+}
